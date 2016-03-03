288a0453b2ff513946fbfb4c24e6faa9ce937298
blanca_dp <blank0302@gmail.com>
2016-03-03 20:28:20 -0300

    tobra

---
+++ historico-obra/src/main/webapp/resources/js/tobra/tobra-controller.js
@@ -0,0 +1,85 @@
+/**
+ * @author bdelpuerto
+ */
+
+'use strict;'
+
+var appObra = angular.module('tobraController', [ 'toaster' ]);
+
+appObra
+.controller(
+		'TobraController',
+		[
+		 '$scope',
+		 'SecurityService',
+		 'TobraService',
+		 '$location',
+		 function($scope, SecurityService, TobraService,
+				 $location) {crearTobra(tobra)
+
+			 $scope.tabs = [
+			                {
+			                	title : "Crear Tipo Obra",
+			                	url : "resources/templates/tobra/crearTobra.html"
+			                },
+			                {
+			                	title : "Buscar Tipo Obra",
+			                	url : "resources/templates/tobra/buscarTobra.html"
+			                } ];
+
+				 $scope.currentTab = 'resources/templates/tobra/crearTobra.html';
+
+				 $scope.onClickTab = function(tab) {
+					 $scope.currentTab = tab.url;
+				 }
+
+				 $scope.isActiveTab = function(tabUrl) {
+					 return tabUrl == $scope.currentTab;
+				 }
+
+				 $scope.logout = function() {
+					 SecurityService.doLogout(function(response) {
+						 $location.url("/");
+					 });
+				 }
+
+		 } ]);
+
+appObra
+.controller(
+		'crearTobraController',
+		[
+		 '$scope',
+		 'TobraService',
+		 'toaster',
+		 function($scope, TobraService, toaster) {
+
+			 $scope.tobra = {};
+
+			 $scope.crearTobra = function(tobra) {
+				 TobraService
+				 .crearTobra(tobra, function(response) {
+							 if (response.success) {
+								 toaster
+								 .pop({
+									 type : 'success',
+									 title : 'Operación correcta.',
+									 body : response.message,
+									 timeout : 3000,
+									 showCloseButton : true
+								 });
+								 $scope.tobra = {};
+							 } else {
+								 toaster
+								 .pop({
+									 type : 'error',
+									 title : 'Error al realizar la operación.',
+									 body : response.message,
+									 timeout : 3000,
+									 showCloseButton : true
+								 });
+							 }
+						 });
+			 };
+
+		 } ])
