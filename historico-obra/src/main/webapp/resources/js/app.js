4678aa236ec9bd3e60b1a7851219c1055e2201d1
blanca_dp <blank0302@gmail.com>
2016-03-05 11:41:54 -0300

    correcciones

--- historico-obra/src/main/webapp/resources/js/app.js
+++ historico-obra/src/main/webapp/resources/js/app.js
@@ -4,15 +4,11 @@
 'use strict';
 
 var app = angular.module("historico-app", [ 'ngRoute', 'loginController',
-<<<<<<< HEAD
 		'usuarioController', 'tobraController', 'ttareaController', 
 		'toaster', 'usuarioServices', 'tobraServices', 'ttareaServices', 'historicoServices', 
-		'angular-loading-bar', 'homeController' ]);
-=======
-		'historicoServices', 'usuarioController', 'toaster', 'usuarioServices',
 		'angular-loading-bar', 'homeController', 'monedaController',
-		'monedaServices', 'smart-table', 'ngDialog']);
->>>>>>> 6df0f2125fddff3f2fa3c567634fcc2b59f01d1c
+		'monedaServices', 'smart-table', 'ngDialog' ]);
+
 
 app.config([ '$routeProvider', function($routeProvider) {
 
@@ -39,14 +35,12 @@ app.config([ '$routeProvider', function($routeProvider) {
 		templateUrl : "resources/templates/home/home.html",
 		controller : "HomeController"
 	})
-<<<<<<< HEAD
-=======
+
 
 	$routeProvider.when("/moneda", {
 		templateUrl : "resources/templates/moneda/moneda.html",
 		controller : "MonedaController"
 	})
->>>>>>> 6df0f2125fddff3f2fa3c567634fcc2b59f01d1c
 	
 	$routeProvider.otherwise({
 		redirectTo : '/'
